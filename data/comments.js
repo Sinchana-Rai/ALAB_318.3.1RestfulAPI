c8f041ca2f3d129257345b9cfb1e3cc7a8612eb9
Sinchana-Rai <sinchana.srai@gmail.com>
2024-11-17 18:28:19 -0500

    comments updated

--- data/comments.js
+++ data/comments.js
@@ -6,20 +6,6 @@ const comments = [
         body:
             "This is first sample comment",
     },
-    {
-        id:1,
-        userId:2,
-        postId:1,
-        body:
-            "This is 2nd sample comment",
-    },
-    {
-        id:1,
-        userId:1,
-        postId:2,
-        body:
-            "This is 3rd sample comment",
-    },
-    ]
+     ]
 
     module.exports = comments;
